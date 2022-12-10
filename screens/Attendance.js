1d5363a12261d6c2a926bb6ac2a6b2332d6996ae
Gereltuya <gereltuya@Gereltuyas-MacBook-Pro.local>
2022-12-11 04:02:41 +0800

    change design of attendance card & add time difference

--- screens/Attendance.js
+++ screens/Attendance.js
@@ -1,51 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, ImageBackground } from 'react-native';
 import { Block, Text } from 'galio-framework';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import EntypoIcons from "react-native-vector-icons/Entypo";
 
 const Attendance = () => {
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+
+  const timedif = () => {
+    const start = "09:10".split(":");
+    const end = "17:00".split(":");
+    const startTime = new Date(0, 0, 0, start[0], start[1], 0);
+    const endTime = new Date(0, 0, 0, end[0], end[1], 0);
+    let diff = endTime.getTime() - startTime.getTime();
+    const hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    const minutes = Math.floor(diff / 1000 / 60);
+    setHours(hours);
+    setMinutes(minutes);
+  }
+
+  useEffect(() => {
+    timedif();
+  }, [])
+
   return (
     <Block style={styles.container}>
       <View>
-        <Text muted style={{ marginBottom: 10 }} size={17}>15 December</Text>
+        <Text muted style={{ marginBottom: 10 }} size={17}>15 December, 2022</Text>
         <ImageBackground source={require('./img/check-in-background.jpg')} resizeMode="cover" style={styles.backgroundImg} imageStyle={{ borderRadius: 10 }}>
-          <Block row style={styles.checkin}>
-            <Text size={12} color="white" style={{ marginRight: 15 }}>Check-In Time</Text>
-            <AntDesignIcons name="checkcircle" color={'#fff'} size={16} />
+          <Block row style={styles.time} space="between">
+            <Block>
+              <Text size={12} color="white" style={{ marginRight: 15, marginBottom: 7 }}>Check-in</Text>
+              <Text h5 color="white">08:48 AM</Text>
+            </Block>
+            <Block>
+              <Text size={12} color="white" style={{ marginRight: 15, marginBottom: 7 }}>Check-out</Text>
+              <Text h5 color="white">05:30 PM</Text>
+            </Block>
           </Block>
-          <Text h3 color="white">08:48 AM</Text>
-          <Text size={10} color="white">Today</Text>
+          <Block row space='between' style={{ marginTop: 15 }}>
+            <Block row middle>
+              <EntypoIcons name="location-pin" size={20} color={'#fff'} />
+              <Text style={{ marginLeft: 6 }} size={11} color="white" bold>Central tower</Text>
+            </Block>
+            <Block row middle>
+              <AntDesignIcons name="checkcircle" color={'#fff'} size={16} />
+              <Text style={{ marginLeft: 6 }} size={11} color='white' bold>{hours}h {minutes}m</Text>
+            </Block>
+          </Block>
         </ImageBackground>
       </View>
-      <ImageBackground source={require('./img/check-out-background.jpg')} resizeMode="cover" style={styles.backgroundImg} imageStyle={{ borderRadius: 10 }}>
-        <Block row style={styles.checkin}>
-          <Text size={12} color="white" style={{ marginRight: 15 }}>Check-Out Time</Text>
-          <AntDesignIcons name="checkcircle" color={'#fff'} size={16} />
-        </Block>
-        <Text h3 color="white">05:00 PM</Text>
-        <Text size={10} color="white">Today</Text>
-      </ImageBackground>
-
-      {/* second round */}
-      <View>
-        <Text muted style={{ marginBottom: 10 }} size={17}>14 December</Text>
-        <ImageBackground source={require('./img/check-in-background.jpg')} resizeMode="cover" style={styles.backgroundImg} imageStyle={{ borderRadius: 10 }}>
-          <Block row style={styles.checkin}>
-            <Text size={12} color="white" style={{ marginRight: 15 }}>Check-In Time</Text>
-            <AntDesignIcons name="checkcircle" color={'#fff'} size={16} />
-          </Block>
-          <Text h3 color="white">09:30 AM</Text>
-          <Text size={10} color="white">Today</Text>
-        </ImageBackground>
-      </View>
-      <ImageBackground source={require('./img/check-out-background.jpg')} resizeMode="cover" style={styles.backgroundImg} imageStyle={{ borderRadius: 10 }}>
-        <Block row style={styles.checkin}>
-          <Text size={12} color="white" style={{ marginRight: 15 }}>Check-Out Time</Text>
-          <AntDesignIcons name="checkcircle" color={'#fff'} size={16} />
-        </Block>
-        <Text h3 color="white">07:00 PM</Text>
-        <Text size={10} color="white">Today</Text>
-      </ImageBackground>
     </Block>
   )
 }
@@ -58,12 +64,9 @@ const styles = StyleSheet.create({
   },
   backgroundImg: {
     justifyContent: 'center',
-    height: 120,
-    paddingHorizontal: 20,
-    marginBottom: 20
+    height: 115,
+    paddingHorizontal: 30,
+    marginBottom: 20,
   },
-  checkin: {
-    alignItems: 'center',
-  }
 });
 export default Attendance
