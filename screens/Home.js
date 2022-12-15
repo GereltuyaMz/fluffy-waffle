12c02265958bf90f4b5754d62799ceb752d746f4
Gereltuya <gereltuya@Gereltuyas-MacBook-Pro.local>
2022-12-16 01:27:09 +0800

    add fancy-alert

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,11 @@
+import React, { useState, useCallback } from 'react';
 import { StyleSheet, Dimensions, FlatList, View, Alert, TouchableOpacity, Image } from 'react-native';
+import { Block, theme, Text, Button } from 'galio-framework';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/core'
-import { Block, theme, Text, Button } from 'galio-framework';
+import { FancyAlert } from 'react-native-expo-fancy-alerts';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
-import React from 'react';
+import IonIcons from "react-native-vector-icons/Ionicons";
 import { auth } from '../firebaseConfig';
 import { Images } from '../constants';
 
@@ -26,8 +28,22 @@ const CardBox = ({ item, navigation }) => {
     </TouchableOpacity>
   )
 }
+
 const Home = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+  const [visible, setVisible] = useState(false);
+  const [endTime, setEndTime] = useState(false);
+
+  const toggleAlert = useCallback(() => {
+    setVisible(!visible);
+  }, [visible]);
+
+  const toggleEndTime = useCallback(() => {
+    setEndTime(!endTime);
+  }, [endTime]);
+
+  const currentDate = new Date().toLocaleString();
+
   return (
     // <SafeAreaView>
     <Block middle style={styles.home}>
@@ -60,15 +76,47 @@ const Home = () => {
         columnWrapperStyle={{ justifyContent: 'space-around' }}
         ListFooterComponent={
           <Block row style={styles.time} card>
-            <Button color="info" onPress={() => Alert.alert('Arrived Time: 9:00 AM')}>
+            <Button color="info" onPress={toggleAlert}>
               ирсэн цаг
             </Button>
-            <Button onPress={() => Alert.alert('Leave Time: 6:00 PM')}>
+            <Button onPress={toggleEndTime}>
               явсан цаг
             </Button>
           </Block>
         }
       />
+      <FancyAlert
+        visible={visible}
+        icon={<View style={{
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#1363DF',
+          borderRadius: 50,
+          width: '100%',
+        }}><IonIcons name="log-in-outline" size={40} color="#fff" /></View>}
+        style={{ backgroundColor: 'white' }}
+      >
+        <Text style={{ marginBottom: 20 }}>You arrived at: {currentDate}</Text>
+        <Button style={{ marginBottom: 25 }} color="info" onPress={toggleAlert}>Close</Button>
+      </FancyAlert>
+      <FancyAlert
+        visible={endTime}
+        icon={<View style={{
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#5E72E4',
+          borderRadius: 50,
+          width: '100%',
+        }}><IonIcons name="log-out-outline" size={40} color="#fff" /></View>}
+        style={{ backgroundColor: 'white' }}
+      >
+        <Text style={{ marginBottom: 20 }}>You left at: {currentDate}</Text>
+        <Button style={{ marginBottom: 25 }} onPress={toggleEndTime}>Close</Button>
+      </FancyAlert>
     </Block>
     // </SafeAreaView>
   )
